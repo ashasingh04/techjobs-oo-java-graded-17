0d32ee705fbeb19e627d34373824c0ea4b9f4953
ashasingh04 <asha.sgsits@gmail.com>
2024-05-09 20:54:35 -0500

    code added

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -48,7 +48,7 @@ public class JobTest {
 
         assertEquals(lineSeparator(),firstChar);
         assertEquals(lineSeparator(),lastChar);
-//        assertEquals("\n",lastChar);
+
     }
 
     @Test
